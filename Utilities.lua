6e0e0d21bb4aefb80c00ffdd4f95b39c6e987f21
kgukmz <144381271+kgukmz@users.noreply.github.com>
2024-05-06 18:51:56 +0100

    Update and rename Utilities to Utilities.lua

---
+++ Utilities.lua
@@ -0,0 +1,31 @@
+local Utilites = {}
+
+function Utilities.MatchExecutor(Executor)
+    if (identifyexecutor():match(Executor)) then
+        return true
+    end
+
+    return false
+end
+
+function Utilities.RandomString(StringLength)
+    local CharSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    local RandomString = ""
+    
+    for i = 1, StringLength do
+        local RandomIndex = math.random(1, #CharSet)
+        RandomString = RandomString .. string.sub(CharSet, RandomIndex, RandomIndex)
+    end
+    
+    return RandomString
+end
+
+function Utilities.MessageBoxNotify(Title, Description, Type)
+    if (not getgenv().messagebox) then
+        return
+    end
+
+    getgenv().messagebox(Title, Description, Type or 0)
+end
+
+return Utilites
